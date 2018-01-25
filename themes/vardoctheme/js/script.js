6239dc509e35d5333144be1ac9a6805df6db38a3
MuathKhraisat <MuathKhraisat@3523826.no-reply.drupal.org>
2018-01-25 20:05:30 +0200

    Issue #2939540 by Omar Alahmed, Muath Khraisat: Changed [Vardoc theme] theme to work with a new style

--- themes/vardoctheme/js/script.js
+++ themes/vardoctheme/js/script.js
@@ -9,12 +9,18 @@
   Drupal.behaviors.vardoctheme = {
     attach: function (context) {
       // Vartheme subtheme JavaScript behaviors goes here.
-
       $('#block-exposedformbook-searchpage-search form').submit(function () {
         if ($.trim($(".form-text").val()) === "") {
           return false;
         }
       });
+      // Adding the title attribute for the sharethis wrapper. 
+      $('.sharethis-wrapper').attr('title', Drupal.t('Share'));
+      // Hiding the notifications count when there are no notifications
+      var count = $('.notifications-count a').text();
+      if (count == 0) {
+        $('.notifications-count').css ('display','none');
+      }
     }
   };
 
